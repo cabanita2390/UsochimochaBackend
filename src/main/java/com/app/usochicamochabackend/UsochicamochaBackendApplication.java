a34461f6ba863e4d0fdae27fbda972a46d340b12
CesarGaviriaS <cesarsanabria321@gmail.com>
2025-11-19 14:26:37 -0500

    main puesto

---
+++ src/main/java/com/app/usochicamochabackend/UsochicamochaBackendApplication.java
@@ -0,0 +1,14 @@
+package com.app.usochicamochabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsochicamochaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsochicamochaBackendApplication.class, args);
+    }
+
+
+}
