036549c601503d338d9e6cfa1f67f2b9fe8255f5
David Ortiz <judaortizchico@gmail.com>
2026-04-13 10:56:39 -0500

    removiendo archivos de logs del repositorio

--- query_docs.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set PGPASSWORD=admin
-"C:\Program Files\PostgreSQL\16\bin\psql.exe" -U postgres -d APP_usuchicamocha3 -c "SELECT v.placa, d.tipo_documento, d.fecha_vencimiento, d.activo FROM documentacion_y_elementos d JOIN vehiculos v ON d.id_vehiculo = v.id WHERE v.tipo_vehiculo = 'MOTOCICLETA' ORDER BY v.placa, d.tipo_documento LIMIT 30;"
